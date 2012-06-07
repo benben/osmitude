66757243c40b36d158c22aa81ef12cc7d853bbfb
benben <ben@nerdlabor.de>
2012-06-07 17:37:52 +0200

    added leaflet map

--- public/app.js
+++ public/app.js
@@ -10,7 +10,6 @@ var out = function(message, type) {
 }
 
 var log = function(message) {
-  console.log(message);
   out(message, 'info');
 };
 
@@ -19,6 +18,27 @@ var err = function(message) {
 }
 
 $(function (){
+  /**************************************/
+  /* MAP STUFF                          */
+  /**************************************/
+
+  $("#map").width($(window).width());
+  $("#map").height($(window).height() - $("#map").offset().top);
+
+  $(window).resize(function() {
+    $("#map").width($(window).width());
+    $("#map").height($(window).height() - $("#map").offset().top);
+  });
+
+  var map = new L.Map('map');
+
+  //var osmUrl = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+  var osmUrl = 'http://{s}tile.cloudmade.com/4a9852ba82264eb9a16c531fd53e70ed/999/256/{z}/{x}/{y}.png',
+  osmAttribution = 'leaflet/cloudmade',
+  osm = new L.TileLayer(osmUrl, {maxZoom: 18, attribution: osmAttribution, subdomains: ["a.", "b.", "c.", ""]});
+
+  map.setView(new L.LatLng(51.338, 12.375), 13).addLayer(osm);
+
   $('#update').click(function(ev){
     ev.preventDefault();
     log("clicked!");
@@ -30,6 +50,7 @@ $(function (){
         var location = jQuery.parseJSON(msg);
         console.log(location);
         log(msg);
+        map.setView(new L.LatLng(location.latitude, location.longitude), 13).addLayer(osm);
       },
       error: function(xhr, ajaxOptions, thrownError){
         err(xhr.status);
